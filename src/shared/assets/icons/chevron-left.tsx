92e31aadaf85130f6d6ab39a9a9af9481d4c2364
Anna Zagrebaylo <107932116+ZenkoWu@users.noreply.github.com>
2024-10-31 00:29:42 +0300

    added main svg icons

---
+++ src/shared/assets/icons/chevron-left.tsx
@@ -0,0 +1,19 @@
+const ChevronLeft = () => (
+  <svg
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M12.5 5L7.5 10L12.5 15"
+      stroke="#98A2B3"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+export default ChevronLeft;
---
+++ src/shared/assets/icons/chevron-right.tsx
@@ -0,0 +1,19 @@
+const ChevronRight = () => (
+  <svg
+    width="20"
+    height="20"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M9 6.00012L15 12.0001L9 18.0001"
+      stroke="#98A2B3"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+export default ChevronRight;
