026424e2df572c1e6124432d310992d0582ba26e
Anna Zagrebaylo <107932116+ZenkoWu@users.noreply.github.com>
2024-10-31 00:39:11 +0300

    added sidebar svgs

---
+++ src/widgets/Sidebar/constants/constants.ts
@@ -0,0 +1,28 @@
+import svg_1 from "../svg/accelerate-svgrepo-com.svg";
+import svg_2 from "../svg/bag-svgrepo-com.svg";
+import svg_3 from "../svg/apple-svgrepo-com.svg";
+import svg_4 from "../svg/cash-svgrepo-com.svg";
+import svg_5 from "../svg/collect-svgrepo-com.svg";
+import svg_6 from "../svg/conversation-svgrepo-com.svg";
+import svg_7 from "../svg/cute-animals-svgrepo-com.svg";
+import svg_8 from "../svg/desk-lamp-svgrepo-com.svg";
+import svg_9 from "../svg/front-page-svgrepo-com.svg";
+import svg_10 from "../svg/grape-svgrepo-com.svg";
+import svg_11 from "../svg/parasol-svgrepo-com.svg";
+import svg_12 from "../svg/penguin-svgrepo-com.svg";
+
+// todo this is a mock data, need to get svg from server :)
+export const SVG_GROUP = [
+  { id: 1, svg: svg_1 },
+  { id: 2, svg: svg_2 },
+  { id: 3, svg: svg_3 },
+  { id: 4, svg: svg_4 },
+  { id: 5, svg: svg_5 },
+  { id: 6, svg: svg_6 },
+  { id: 7, svg: svg_7 },
+  { id: 8, svg: svg_8 },
+  { id: 9, svg: svg_9 },
+  { id: 10, svg: svg_10 },
+  { id: 11, svg: svg_11 },
+  { id: 12, svg: svg_12 },
+];
